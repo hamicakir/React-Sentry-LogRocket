deee3bdb53213ed202f307afb60a8ec656855a65
Ünal Sürmeli <unal.surmeli@iyzi-sfd-46.local>
2019-03-13 10:59:59 +0300

    Logrocket integration completed.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import LogRocket from 'logrocket';
+
+LogRocket.identify('jstanbul-presentation/jstanbul-presentation', {
+    name: 'James Morrison',
+    email: 'jamesmorrison@example.com',
+
+    // Add your own custom user variables here, ie:
+    subscriptionType: 'pro'
+});
+
+LogRocket.init('jstanbul-presentation/jstanbul-presentation');
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
