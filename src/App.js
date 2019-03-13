8c5824e340315141be32aac665bd7f1e5f8d97f1
Hami Çakır <hami.cakr@iyzi-sfd-32.local>
2019-03-13 13:58:15 +0300

    Design and sentry integration completed.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as Sentry from '@sentry/browser';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state={
+        error: null,
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({ error });
+        Sentry.withScope(scope => {
+            Object.keys(errorInfo).forEach(key => {
+                scope.setExtra(key, errorInfo[key]);
+            });
+            Sentry.captureException(error);
+        });
+    }
+
+    evalClickHandler = () => {
+        try {
+            const code = "Hi zihni";
+            eval(code);
+        } catch (e) {
+            Sentry.captureException(e);
+        }
+    }
+
+    rangeClickHandler = () => {
+        try
+        {
+            const b = new Array(-1); //range error
+        }
+        catch(error)
+        {
+            Sentry.captureException(error);
+        }
+    }
+
+    referenceClickHandler = () => {
+        try {
+            throw new ReferenceError('Hello', 'someFile.js', 10);
+        } catch (e) {
+            Sentry.captureException(e);
+        }
+    }
+
+    syntaxClickHandler = () => {
+        try {
+            eval('hoo bar');
+        } catch (e) {
+            Sentry.captureException(e);                 // "@Scratchpad/1:2:3\n"
+        }
+    }
+
+    typeClickHandler = () => {
+        try{
+            const foo = {
+                apple: 5,
+                banana: 8
+            }
+            foo.map(f => f * 5);
+        }catch(e){
+            Sentry.captureException(e);
+        }
+    }
+
+    uriClickHandler = () => {
+        try{
+            decodeURIComponent('%E0%A4%A');
+        }catch(e){
+            Sentry.captureException(e);
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>
+                        We love errors..
+                    </h1>
+                </header>
+                <div>
+                    <button type="button" onClick={this.evalClickHandler}>Eval Error</button>
+                    <button type="button" onClick={this.rangeClickHandler}>Range Error</button>
+                    <button type="button" onClick={this.referenceClickHandler}>Reference Error</button>
+                    <button type="button" onClick={this.syntaxClickHandler}>Syntax Error</button>
+                    <button type="button" onClick={this.typeClickHandler}>Type Error</button>
+                    <button type="button" onClick={this.uriClickHandler}>URI Error</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import LogRocket from 'logrocket';
+import * as Sentry from '@sentry/browser';
+
+import packageJson from '../package.json';
+
+Sentry.init({
+    dsn: "https://16025aff21114f7b837f10da3f43ee77@sentry.io/1414258",
+    release: packageJson.version,
+});
+
+LogRocket.identify('jstanbul-presentation/jstanbul-presentation', {
+    name: 'James Morrison' + Math.random() * 1000,
+    email: 'jamesmorrison@example.com',
+
+    // Add your own custom user variables here, ie:
+    subscriptionType: 'pro'
+});
+
+LogRocket.init('jstanbul-presentation/jstanbul-presentation');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
